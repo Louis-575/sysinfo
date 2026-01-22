b06d2e64b19e3149093192970522bfdbdc8db1df
lou <louismann86@gmail.com>
2026-01-22 00:07:46 +0000

    Added uptime output

--- sysinfo.py
+++ sysinfo.py
@@ -1,3 +1,8 @@
 import socket
+import subprocess
 hostname = socket.gethostname()
+
+hostname = socket.gethostname()
+uptime = subprocess.check_output(["uptime","-p"]).decode().strip()
 print(f"Hostname: {hostname}")
+print(f"Uptime: {uptime}")
