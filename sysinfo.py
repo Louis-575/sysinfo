230c8cd676b6df66aad652072216373a6de61346
lou <louismann86@gmail.com>
2026-01-22 00:13:51 +0000

    Added drive usage output

--- sysinfo.py
+++ sysinfo.py
@@ -1,8 +1,11 @@
 import socket
 import subprocess
+import shutil
 hostname = socket.gethostname()
 
 hostname = socket.gethostname()
 uptime = subprocess.check_output(["uptime","-p"]).decode().strip()
+disk = shutil.disk_usage("/driveb/")
 print(f"Hostname: {hostname}")
 print(f"Uptime: {uptime}")
+print(f"Drive b disk usage: {disk.used // (1024**3)} GB / {disk.total // (1024**3)} GB")
